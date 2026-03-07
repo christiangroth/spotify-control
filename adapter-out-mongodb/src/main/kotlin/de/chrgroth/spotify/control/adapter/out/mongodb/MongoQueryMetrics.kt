f1af0a622c1625dbff5128be5577df7cd26de30c
Christian Groth <2071856+christiangroth@users.noreply.github.com>
2026-03-07 18:24:16 +0100

    MongoQueryMetrics.kt aktualisieren

--- adapter-out-mongodb/src/main/kotlin/de/chrgroth/spotify/control/adapter/out/mongodb/MongoQueryMetrics.kt
+++ adapter-out-mongodb/src/main/kotlin/de/chrgroth/spotify/control/adapter/out/mongodb/MongoQueryMetrics.kt
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit
 @ApplicationScoped
 class MongoQueryMetrics(
     private val meterRegistry: MeterRegistry,
-    @param:ConfigProperty(name = "app.mongodb.slow-query-threshold-ms", defaultValue = "50")
+    @param:ConfigProperty(name = "app.mongodb.slow-query-threshold-ms", defaultValue = "100")
     private val slowQueryThresholdMs: Long,
 ) {
 
